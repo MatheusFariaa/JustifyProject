08c25c75071d5dba9afcbf1661e61d80d1c10424
Fariasmlk <matheusfaria77@uni9.edu.br>
2023-01-12 23:35:29 -0300

    iniciando projeto/aula1

---
+++ src/environments/environments.ts
@@ -0,0 +1,19 @@
+export const environments = {
+  production: false
+};
+
+export const JustifyConfiguration = {
+  clientId: 'dbcadd915af64307b96ac48ca499f38f',
+  authEndpoint:'https://accounts.spotify.com/authorize',
+  redirectUrl:'https://localhost:4200/login/',
+  scopes: [
+    "user-read-currently-playing", // musica tocando agora.
+    "user-read-recently-played", // ler musicas tocadas recentemente
+    "user-read-playback-state", // ler estado do player do usuario
+    "user-top-read", // top artistas e musicas do usuario
+    "user-modify-playback-state", // alterar do player do usuario.
+    "user-library-read", // ler biblioteca dos usuarios
+    "playlist-read-private", // ler playlists privads
+    "playlist-read-collaborative" // ler playlists colaborativas
+  ]
+}
